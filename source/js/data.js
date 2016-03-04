ab5e5c1c5f26248d00c29da7e816277a3f37abca
wesbos <wesbos@gmail.com>
2016-03-03 21:26:37 -0500

    ITS ALIVE

---
+++ source/js/data.js
@@ -0,0 +1,71 @@
+module.exports = {
+  videos : [
+
+    {
+      "title": "Mastering Markdown — Course Trailer",
+      "id": "Z4fleRPRAXw",
+      "duration": "0:46",
+      "number": 0
+    },
+    {
+      "title": "Introduction to Markdown & The Tooling",
+      "id": "8YC4kw9w89E",
+      "duration": "6:55",
+      "number": 1
+    },
+    {
+      "title": "Paragraphs and Text Decoration",
+      "id": "6EU1a_cniWU",
+      "duration": "3:04",
+      "number": 2
+    },
+    {
+      "title": "Creating + Linking Headings",
+      "id": "_X4EMz0A__g",
+      "duration": "2:06",
+      "number": 3
+    },
+    {
+      "title": "Four Ways to Make Links",
+      "id": "OQtZLHanB_U",
+      "duration": "3:36",
+      "number": 4
+    },
+    {
+      "title": "Markdown Images",
+      "id": "Mh0Of4LzEfk",
+      "duration": "4:17",
+      "number": 5
+    },
+    {
+      "title": "Lists — Ordered, Unordered, Bullets and Nesting",
+      "id": "NMoVT04wHGc",
+      "duration": "3:59",
+      "number": 6
+    },
+    {
+      "title": "Line Breaks, Horizontal Rules and Blockquotes",
+      "id": "dpsKi8a1AKI",
+      "duration": "2:43",
+      "number": 7
+    },
+    {
+      "title": "Code Blocks + Syntax Highlighting",
+      "id": "9eucl-HyNAM",
+      "duration": "3:14",
+      "number": 8
+    },
+    {
+      "title": "Tables",
+      "id": "3vXkgWtON9M",
+      "duration": "1:59",
+      "number": 9
+    },
+    {
+      "title": "Github Treats",
+      "id": "ziSkbxWwDIQ",
+      "duration": "1:49",
+      "number": 10
+    }
+  ] // end videos array
+}
